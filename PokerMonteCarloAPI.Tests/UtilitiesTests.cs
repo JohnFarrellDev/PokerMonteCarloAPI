3b495380a9679cf67d28fffbbc15b4ad190e2a15
John Farrell <johnfarrell@fastmail.com>
2023-04-09 01:21:36 +0100

    Refactor tests to use capitalized property names for card suit and value

--- PokerMonteCarloAPI.Tests/UtilitiesTests.cs
+++ PokerMonteCarloAPI.Tests/UtilitiesTests.cs
@@ -21,24 +21,24 @@ namespace PokerMonteCarloAPI.Tests
             allCards.Count.Should().Be(52);
             allCardsSet.Count.Should().Be(52);
 
-            allCardsSet.Count(card => card.suit == Suit.Clubs).Should().Be(13);
-            allCardsSet.Count(card => card.suit == Suit.Diamonds).Should().Be(13);
-            allCardsSet.Count(card => card.suit == Suit.Hearts).Should().Be(13);
-            allCardsSet.Count(card => card.suit == Suit.Spades).Should().Be(13);
+            allCardsSet.Count(card => card.Suit == Suit.Clubs).Should().Be(13);
+            allCardsSet.Count(card => card.Suit == Suit.Diamonds).Should().Be(13);
+            allCardsSet.Count(card => card.Suit == Suit.Hearts).Should().Be(13);
+            allCardsSet.Count(card => card.Suit == Suit.Spades).Should().Be(13);
             
-            allCardsSet.Count(card => card.value == Value.Two).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Three).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Four).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Five).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Six).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Seven).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Eight).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Nine).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Ten).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Jack).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Queen).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.King).Should().Be(4);
-            allCardsSet.Count(card => card.value == Value.Ace).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Two).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Three).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Four).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Five).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Six).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Seven).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Eight).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Nine).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Ten).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Jack).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Queen).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.King).Should().Be(4);
+            allCardsSet.Count(card => card.Value == Value.Ace).Should().Be(4);
         }
 
         [Test]
