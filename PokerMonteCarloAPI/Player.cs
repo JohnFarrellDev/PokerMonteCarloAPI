14d331ba994e316a2d513cc8d186d2a7576683e8
John Farrell <johnfarrell@fastmail.com>
2023-04-09 01:22:20 +0100

    Refactor Player class to use byte instead of enum for value and suit counts, and list of bytes instead of cards for sorted descending values

--- PokerMonteCarloAPI/Player.cs
+++ PokerMonteCarloAPI/Player.cs
@@ -8,44 +8,39 @@ namespace PokerMonteCarloAPI
     {
         private readonly List<Card> _playersHand;
         private readonly bool _folded;
-        private readonly Dictionary<Value, byte> _valueCounts;
-        private readonly Dictionary<Suit, byte> _suitCounts;
-        private readonly List<Value> _sortedDescending;
-        private readonly Dictionary<byte, byte> _sameValueCount;
-
+        private readonly Dictionary<byte, byte> _valueCounts;
+        private readonly Dictionary<byte, byte> _suitCounts;
+        private readonly List<byte> _sortedDescending;
 
         public Player(List<Card> playersHand, bool folded)
         {
             _playersHand = playersHand;
             _folded = folded;
-            _valueCounts = new Dictionary<Value, byte>();
-            _suitCounts = new Dictionary<Suit, byte>();
+            _valueCounts = new Dictionary<byte, byte>();
+            _suitCounts = new Dictionary<byte, byte>();
             foreach (var card in playersHand)
             {
-                if (!_valueCounts.ContainsKey(card.value))
+                if (!_valueCounts.ContainsKey(card.Value))
                 {
-                    _valueCounts[card.value] = 0;
+                    _valueCounts[card.Value] = 0;
                 }
-                _valueCounts[card.value]++;
-                if (!_suitCounts.ContainsKey(card.suit))
+                _valueCounts[card.Value]++;
+                if (!_suitCounts.ContainsKey(card.Suit))
                 {
-                    _suitCounts[card.suit] = 0;
+                    _suitCounts[card.Suit] = 0;
                 }
-                _suitCounts[card.suit]++;
+                _suitCounts[card.Suit]++;
             }
-            _sortedDescending = playersHand.Select(c => c.value).OrderByDescending(v => v).ToList();
-            _sameValueCount = new Dictionary<byte, byte>();
+            _sortedDescending = playersHand.Select(c => c.Value).OrderDescending().ToList();
         }
 
-        public (Hand, List<Card>) CalculateBestHand()
+        public (Hand, List<byte>) CalculateBestHand()
         {
             // calculate the best poker hand possible from the PLayersHand and the table cards
             // return the best hand and the high cards in order
 
             var (hasFlush, anyFlushHighCards, flushType) = HasAnyFlush();
-
             
-
             // calculate if the List<Card> contains a royal flush (DONE)
             if (flushType == Hand.RoyalFlush) return (Hand.RoyalFlush, anyFlushHighCards!);
             
@@ -61,50 +56,50 @@ namespace PokerMonteCarloAPI
             
             // calculate if the List<Card> contains a flush (DONE)
             if (hasFlush) return (Hand.Flush, anyFlushHighCards!);
+            
             // calculate if the List<Card> contains a straight (DONE)
+            
+            
             // calculate if the List<Card> contains a three of a kind
+            
+            
             // calculate if the List<Card> contains a two pair
+            
+            
             // calculate if the List<Card> contains a pair
+            
+            
             // calculate if the List<Card> contains a high card
-
-
-
         }
 
         // flushCards will always be belonging to the same Suit
-        private static (bool, List<Card>) HasRoyalFlush(List<Card> flushCards)
+        private static (bool, List<byte>?) HasRoyalFlush(List<Card> flushCards)
         {
-            var royalFlush = new List<Value> { Value.Ten, Value.Jack, Value.Queen, Value.King, Value.Ace };
-            var isRoyalFlush = royalFlush.All(v => flushCards.Select(c => c.value).Contains(v));
+            var royalFlush = new List<byte> { 10,11, 12, 13, 14 };
+            var isRoyalFlush = royalFlush.All(v => flushCards.Select(c => c.Value).Contains(v));
 
             if (isRoyalFlush)
             {
-                return (true, new List<Card>
+                return (true, new List<byte>
                 {
-                    flushCards.Find(v => v.value == Value.Ace)!,
-                    flushCards.Find(v => v.value == Value.King)!,
-                    flushCards.Find(v => v.value == Value.Queen)!,
-                    flushCards.Find(v => v.value == Value.Jack)!,
-                    flushCards.Find(v => v.value == Value.Ten)!,
+                    14,
+                    13,
+                    12,
+                    11,
+                    10,
                 });
             }
 
-            return (false, new List<Card>());
+            return (false, null);
         }
 
         // flushCards will always be belonging to the same Suit
-        public (bool, List<Card>?) HasStraightFlush(List<Card> flushCards)
+        public (bool, List<byte>?) HasStraightFlush(List<Card> flushCards)
         {
-            var sortedValues = flushCards.Select(c => c.value).Distinct().OrderByDescending(v => v).ToList();
-            var straightValues = new List<Value>();
-    
-            // Check for Ace low straight
-            var aceLowStraight = new List<Value>() { Value.Five, Value.Four, Value.Three, Value.Two, Value.Ace };
-            if (aceLowStraight.All(sortedValues.Contains))
-            {
-                straightValues = new List<Value>() { Value.Ace, Value.Two, Value.Three, Value.Four, Value.Five };
-            }
-
+            var sortedValues = flushCards.Select(c => c.Value).Distinct().OrderByDescending(b => b).ToList();
+            // in case of Ace low straight
+            if(sortedValues[0] == 14) sortedValues.Add(1);
+            
             // Check for regular straight
             for (var i = 0; i < sortedValues.Count - 4; i++)
             {
@@ -112,20 +107,15 @@ namespace PokerMonteCarloAPI
                 if (sortedValues[i + 1] != currentVal - 1 || sortedValues[i + 2] != currentVal - 2
                                                           || sortedValues[i + 3] != currentVal - 3 ||
                                                           sortedValues[i + 4] != currentVal - 4) continue;
-                straightValues = new List<Value>() { sortedValues[i], sortedValues[i + 1], sortedValues[i + 2], sortedValues[i + 3], sortedValues[i + 4] };
-                break;
+                return (true,
+                    new List<byte>
+                    {
+                        sortedValues[i], sortedValues[i + 1], sortedValues[i + 2], sortedValues[i + 3],
+                        sortedValues[i + 4]
+                    });
             }
-
-            if(straightValues.Count == 0) return (false, null);
             
-            return (true, new List<Card>()
-            {
-                flushCards.Find(v => v.value == straightValues[0])!,
-                flushCards.Find(v => v.value == straightValues[1])!,
-                flushCards.Find(v => v.value == straightValues[2])!,
-                flushCards.Find(v => v.value == straightValues[3])!,
-                flushCards.Find(v => v.value == straightValues[4])!,
-            });
+            return (false, null);
         }
         
         public (bool, List<Card>?) HasFourOfAKind()
@@ -148,11 +138,11 @@ namespace PokerMonteCarloAPI
             
             var x = (int)fourOfAKindRank
     
-            var fourOfAKindCards = _playersHand.Where(card => card.value == fourOfAKindRank).ToList();
+            var fourOfAKindCards = _playersHand.Where(card => card.Value == fourOfAKindRank).ToList();
     
             // Get the highest card that is not part of the four of a kind
             var highestCard = _playersHand.Where(card => card.rank != fourOfAKindRank)
-                .OrderByDescending(card => card.value)
+                .OrderByDescending(card => card.Value)
                 .FirstOrDefault();
     
             // Add the highest card to the four of a kind
@@ -221,23 +211,23 @@ namespace PokerMonteCarloAPI
         }
 
         
-        public (bool, List<Card>?, Hand?) HasAnyFlush()
+        public (bool, List<byte>?, Hand?) HasAnyFlush()
         {
-            Suit? flushSuit = null;
-            foreach (var count in this._suitCounts)
+            byte flushSuit = 5;
+            foreach (var count in _suitCounts)
             {
                 if (count.Value < 5) continue;
                 flushSuit = count.Key;
                 break;
             }
             
-            if (flushSuit == null)
+            if (flushSuit == 5)
             {
                 return (false, null, null);
             }
             
             // Get the cards of the flush suit
-            var flushCards = _playersHand.Where(card => card.suit == flushSuit).ToList();
+            var flushCards = _playersHand.Where(card => card.Suit == flushSuit).ToList();
 
             // check for royal flush
             var (hasRoyalFlush, royalFlushCards) = HasRoyalFlush(flushCards);
@@ -254,7 +244,7 @@ namespace PokerMonteCarloAPI
             }
             
             // Return true and the 5 highest cards of the flush suit
-            return (true, flushCards.OrderByDescending(card => card.value)
+            return (true, straightFlushCards!.OrderByDescending(v => v)
                 .Take(5).ToList(), Hand.Flush);
         }
 
@@ -286,11 +276,11 @@ namespace PokerMonteCarloAPI
             
             return (true, new List<Card>()
             {
-                _playersHand.Find(v => v.value == straightValues[0])!,
-                _playersHand.Find(v => v.value == straightValues[1])!,
-                _playersHand.Find(v => v.value == straightValues[2])!,
-                _playersHand.Find(v => v.value == straightValues[3])!,
-                _playersHand.Find(v => v.value == straightValues[4])!,
+                _playersHand.Find(v => v.Value == straightValues[0])!,
+                _playersHand.Find(v => v.Value == straightValues[1])!,
+                _playersHand.Find(v => v.Value == straightValues[2])!,
+                _playersHand.Find(v => v.Value == straightValues[3])!,
+                _playersHand.Find(v => v.Value == straightValues[4])!,
             });
         }
 
