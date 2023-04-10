740caf85d74e1a076b9e9d243186c89b08459228
John Farrell <johnfarrell@fastmail.com>
2023-04-10 19:05:57 +0100

    Delete unused class Calculate and add multithreading to Monte Carlo simulation

--- PokerMonteCarloAPI/Calculate.cs
+++
@@ -1,6 +0,0 @@
-namespace PokerMonteCarloAPI;
-
-public class Calculate
-{
-    
-}
--- PokerMonteCarloAPI/Monte.cs
+++ PokerMonteCarloAPI/Monte.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace PokerMonteCarloAPI
 {
@@ -8,132 +9,148 @@ namespace PokerMonteCarloAPI
     {
         public List<PlayerResult> Carlo(Request request)
         {
+
             const int numberOfSimulations = 1_000_000;
             var allCards = Utilities.GenerateAllCards().ToList();
             var remainingCards = RemovePlayerAndTableCards(allCards, request);
-            
-            // calculate each players best hand
-            // first best hand but also 5 highest cards that make the hand
-            // determine winner
-            // keep score for how often each player has won
-            // extra - keep track of each players hand over time, return what percentage of time we got certain hands
-            
+
+            var lockObject = new object();
             var winTracker = new List<PlayerResult>();
+
             for (var i = 0; i < request.Players.Count; i++)
             {
                 winTracker.Add(new PlayerResult());
             }
             
-            for (var i = 0; i < numberOfSimulations; i++)
+            var numberOfTasks = Environment.ProcessorCount;
+            var simulationsPerThread = numberOfSimulations / numberOfTasks;
+            
+            var tasks = new Task[numberOfTasks];
+            for (var i = 0; i < numberOfTasks; i++)
             {
-                var shuffledRemainingCards = remainingCards.ToList().FisherYatesShuffle();
-                var tableCards = Utilities.GenerateTableCards(request, shuffledRemainingCards);
-                var players = Utilities.GeneratePlayers(tableCards, shuffledRemainingCards, request);
-                var playersBestHands = players.Select(player => player.CalculateBestHand()).ToList();
-                
-                var winningIndexes = new List<byte>{0};    
-                
-                for (byte j = 1; j < playersBestHands.Count; j++)
+                tasks[i] = Task.Factory.StartNew(() =>
                 {
-                    var currentWinner = playersBestHands[winningIndexes[0]];
-                    var currentPlayer = playersBestHands[j];
-                    
-                    if(currentPlayer.Item1 < currentWinner.Item1) continue;
-                    if (currentPlayer.Item1 > currentWinner.Item1)
+                    for (var j = 0; j < simulationsPerThread; j++)
                     {
-                        winningIndexes.Clear();
-                        winningIndexes.Add(j);
-                        continue;
-                    }
-                    for (var k = 0; k < currentPlayer.Item2.Count; k++)
-                    {
-                        if(currentPlayer.Item2[k] < currentWinner.Item2[k]) break;
-                        
-                        if (currentPlayer.Item2[k] > currentWinner.Item2[k])
+                        var shuffledRemainingCards = remainingCards.ToList().FisherYatesShuffle();
+                        var tableCards = Utilities.GenerateTableCards(request, shuffledRemainingCards);
+                        var players = Utilities.GeneratePlayers(tableCards, shuffledRemainingCards, request);
+                        var playersBestHands = players.Select(player => player.CalculateBestHand()).ToList();
+
+                        var winningIndexes = new List<byte> { 0 };
+
+                        for (byte k = 1; k < playersBestHands.Count; k++)
                         {
-                            winningIndexes.Clear();
-                            winningIndexes.Add(j);
-                            break;
+                            var currentWinner = playersBestHands[winningIndexes[0]];
+                            var currentPlayer = playersBestHands[k];
+
+                            if (currentPlayer.Item1 < currentWinner.Item1) continue;
+                            if (currentPlayer.Item1 > currentWinner.Item1)
+                            {
+                                winningIndexes.Clear();
+                                winningIndexes.Add(k);
+                                continue;
+                            }
+
+                            for (var l = 0; l < currentPlayer.Item2.Count; l++)
+                            {
+                                if (currentPlayer.Item2[l] < currentWinner.Item2[l]) break;
+
+                                if (currentPlayer.Item2[l] > currentWinner.Item2[l])
+                                {
+                                    winningIndexes.Clear();
+                                    winningIndexes.Add(k);
+                                    break;
+                                }
+
+                                if (l == currentPlayer.Item2.Count - 1)
+                                {
+                                    winningIndexes.Add(k);
+                                }
+                            }
                         }
-                        
-                        if (k == currentPlayer.Item2.Count - 1)
+
+                        lock (lockObject)
                         {
-                            winningIndexes.Add(j);
+                            switch (winningIndexes.Count)
+                        {
+                            case 1:
+                                winTracker[winningIndexes[0]].WinCount++;
+                                break;
+                            case 2:
+                                winTracker[winningIndexes[0]].TwoWayTieCount++;
+                                winTracker[winningIndexes[1]].TwoWayTieCount++;
+                                break;
+                            case 3:
+                                winTracker[winningIndexes[0]].ThreeWayTieCount++;
+                                winTracker[winningIndexes[1]].ThreeWayTieCount++;
+                                winTracker[winningIndexes[2]].ThreeWayTieCount++;
+                                break;
+                            case 4:
+                                winTracker[winningIndexes[0]].FourWayTieCount++;
+                                winTracker[winningIndexes[1]].FourWayTieCount++;
+                                winTracker[winningIndexes[2]].FourWayTieCount++;
+                                winTracker[winningIndexes[3]].FourWayTieCount++;
+                                break;
+                            case 5:
+                                winTracker[winningIndexes[0]].FiveWayTieCount++;
+                                winTracker[winningIndexes[1]].FiveWayTieCount++;
+                                winTracker[winningIndexes[2]].FiveWayTieCount++;
+                                winTracker[winningIndexes[3]].FiveWayTieCount++;
+                                winTracker[winningIndexes[4]].FiveWayTieCount++;
+                                break;
+                            case 6:
+                                winTracker[winningIndexes[0]].SixWayTieCount++;
+                                winTracker[winningIndexes[1]].SixWayTieCount++;
+                                winTracker[winningIndexes[2]].SixWayTieCount++;
+                                winTracker[winningIndexes[3]].SixWayTieCount++;
+                                winTracker[winningIndexes[4]].SixWayTieCount++;
+                                winTracker[winningIndexes[5]].SixWayTieCount++;
+                                break;
+                            case 7:
+                                winTracker[winningIndexes[0]].SevenWayTieCount++;
+                                winTracker[winningIndexes[1]].SevenWayTieCount++;
+                                winTracker[winningIndexes[2]].SevenWayTieCount++;
+                                winTracker[winningIndexes[3]].SevenWayTieCount++;
+                                winTracker[winningIndexes[4]].SevenWayTieCount++;
+                                winTracker[winningIndexes[5]].SevenWayTieCount++;
+                                winTracker[winningIndexes[6]].SevenWayTieCount++;
+                                break;
+                            case 8:
+                                winTracker[winningIndexes[0]].EightWayTieCount++;
+                                winTracker[winningIndexes[1]].EightWayTieCount++;
+                                winTracker[winningIndexes[2]].EightWayTieCount++;
+                                winTracker[winningIndexes[3]].EightWayTieCount++;
+                                winTracker[winningIndexes[4]].EightWayTieCount++;
+                                winTracker[winningIndexes[5]].EightWayTieCount++;
+                                winTracker[winningIndexes[6]].EightWayTieCount++;
+                                winTracker[winningIndexes[7]].EightWayTieCount++;
+                                break;
+                            case 9:
+                                winTracker[winningIndexes[0]].NineWayTieCount++;
+                                winTracker[winningIndexes[1]].NineWayTieCount++;
+                                winTracker[winningIndexes[2]].NineWayTieCount++;
+                                winTracker[winningIndexes[3]].NineWayTieCount++;
+                                winTracker[winningIndexes[4]].NineWayTieCount++;
+                                winTracker[winningIndexes[5]].NineWayTieCount++;
+                                winTracker[winningIndexes[6]].NineWayTieCount++;
+                                winTracker[winningIndexes[7]].NineWayTieCount++;
+                                winTracker[winningIndexes[8]].NineWayTieCount++;
+                                break;
+
                         }
+                        }
+
                     }
-                }
-                
-                switch (winningIndexes.Count)
-                    {
-                        case 1:
-                            winTracker[winningIndexes[0]].WinCount++;
-                            break;
-                        case 2:
-                            winTracker[winningIndexes[0]].TwoWayTieCount++;
-                            winTracker[winningIndexes[1]].TwoWayTieCount++;
-                            break;
-                        case 3:
-                            winTracker[winningIndexes[0]].ThreeWayTieCount++;
-                            winTracker[winningIndexes[1]].ThreeWayTieCount++;
-                            winTracker[winningIndexes[2]].ThreeWayTieCount++;
-                            break;
-                        case 4:
-                            winTracker[winningIndexes[0]].FourWayTieCount++;
-                            winTracker[winningIndexes[1]].FourWayTieCount++;
-                            winTracker[winningIndexes[2]].FourWayTieCount++;
-                            winTracker[winningIndexes[3]].FourWayTieCount++;
-                            break;
-                        case 5:
-                            winTracker[winningIndexes[0]].FiveWayTieCount++;
-                            winTracker[winningIndexes[1]].FiveWayTieCount++;
-                            winTracker[winningIndexes[2]].FiveWayTieCount++;
-                            winTracker[winningIndexes[3]].FiveWayTieCount++;
-                            winTracker[winningIndexes[4]].FiveWayTieCount++;
-                            break;
-                        case 6:
-                            winTracker[winningIndexes[0]].SixWayTieCount++;
-                            winTracker[winningIndexes[1]].SixWayTieCount++;
-                            winTracker[winningIndexes[2]].SixWayTieCount++;
-                            winTracker[winningIndexes[3]].SixWayTieCount++;
-                            winTracker[winningIndexes[4]].SixWayTieCount++;
-                            winTracker[winningIndexes[5]].SixWayTieCount++;
-                            break;
-                        case 7:
-                            winTracker[winningIndexes[0]].SevenWayTieCount++;
-                            winTracker[winningIndexes[1]].SevenWayTieCount++;
-                            winTracker[winningIndexes[2]].SevenWayTieCount++;
-                            winTracker[winningIndexes[3]].SevenWayTieCount++;
-                            winTracker[winningIndexes[4]].SevenWayTieCount++;
-                            winTracker[winningIndexes[5]].SevenWayTieCount++;
-                            winTracker[winningIndexes[6]].SevenWayTieCount++;
-                            break;
-                        case 8:
-                            winTracker[winningIndexes[0]].EightWayTieCount++;
-                            winTracker[winningIndexes[1]].EightWayTieCount++;
-                            winTracker[winningIndexes[2]].EightWayTieCount++;
-                            winTracker[winningIndexes[3]].EightWayTieCount++;
-                            winTracker[winningIndexes[4]].EightWayTieCount++;
-                            winTracker[winningIndexes[5]].EightWayTieCount++;
-                            winTracker[winningIndexes[6]].EightWayTieCount++;
-                            winTracker[winningIndexes[7]].EightWayTieCount++;
-                            break;
-                        case 9:
-                            winTracker[winningIndexes[0]].NineWayTieCount++;
-                            winTracker[winningIndexes[1]].NineWayTieCount++;
-                            winTracker[winningIndexes[2]].NineWayTieCount++;
-                            winTracker[winningIndexes[3]].NineWayTieCount++;
-                            winTracker[winningIndexes[4]].NineWayTieCount++;
-                            winTracker[winningIndexes[5]].NineWayTieCount++;
-                            winTracker[winningIndexes[6]].NineWayTieCount++;
-                            winTracker[winningIndexes[7]].NineWayTieCount++;
-                            winTracker[winningIndexes[8]].NineWayTieCount++;
-                            break;
-                    }
+                });
             }
+
+            Task.WaitAll(tasks);
             
             return winTracker;
         }
 
+        
         private List<Card> RemovePlayerAndTableCards(IEnumerable<Card> allCards, Request request)
         {
             var setOfAllCards = new HashSet<Card>(allCards);
